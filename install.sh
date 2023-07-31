63db6bd376365100546b3609c23db68e7360e499
Aremu Smog <aremuoluwagbamila@gmail.com>
2023-07-31 22:35:18 +0100

    ao_feat: update installation script

--- install.sh
+++ install.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
+echo   echo "                   dddddddd                                   
+  iiii              d::::::d                                   
+ i::::i             d::::::d                                   
+  iiii              d::::::d                                   
+                    d:::::d                                    
+iiiiiii     ddddddddd:::::d   aaaaaaaaaaaaa  nnnn  nnnnnnnn    
+i:::::i   dd::::::::::::::d   a::::::::::::a n:::nn::::::::nn  
+ i::::i  d::::::::::::::::d   aaaaaaaaa:::::an::::::::::::::nn 
+ i::::i d:::::::ddddd:::::d            a::::ann:::::::::::::::n
+ i::::i d::::::d    d:::::d     aaaaaaa:::::a  n:::::nnnn:::::n
+ i::::i d:::::d     d:::::d   aa::::::::::::a  n::::n    n::::n
+ i::::i d:::::d     d:::::d  a::::aaaa::::::a  n::::n    n::::n
+ i::::i d:::::d     d:::::d a::::a    a:::::a  n::::n    n::::n
+i::::::id::::::ddddd::::::dda::::a    a:::::a  n::::n    n::::n
+i::::::i d:::::::::::::::::da:::::aaaa::::::a  n::::n    n::::n
+i::::::i  d:::::::::ddd::::d a::::::::::aa:::a n::::n    n::::n
+iiiiiiii   ddddddddd   ddddd  aaaaaaaaaa  aaaa nnnnnn    nnnnnn
+"
+
+echo "Installing...."
 if [ ! -d ~/bin ]; then
 mkdir ~/bin
 fi
 cp idan ~/bin/idan
 chmod u+x ~/bin/idan
+echo ""
+echo "Installed."
+echo ""
+echo "🛑 Just one more thing..."
+echo "Add the following to your .bash_profile or .zprofile (if you have zsh installed)"
+echo ""
+echo 'export PATH="$HOME/bin:$PATH"'
